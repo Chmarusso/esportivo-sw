daf1153747ef4f8461ade449a92a7df300aa4bfe
Artur Chmaro <artur.chmaro@consileon.pl>
2018-12-01 16:29:17 +0100

    gradient, user screen

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -8,13 +8,12 @@ export default class User extends Component {
       <Container>
         <Row>
           <Col sm="12" md={{ size: 6, offset: 3 }}>
-            <h5>Login</h5>
-            <InputGroup>
+            <h1 style={{textAlign: 'center'}}>Welcome!</h1>
+            <InputGroup style={{padding: '20px'}}>
               <InputGroupAddon addonType="prepend">@</InputGroupAddon>
               <Input placeholder="username" />
             </InputGroup>
-            <h5>Sex</h5>
-            <ButtonGroup>
+            <ButtonGroup style={{display: 'flex', justifyContent: 'center', padding: '20px'}}>
               <Button color="primary">Male</Button>
               <Button color="primary">Female</Button>
               <Button color="primary">Other</Button>
