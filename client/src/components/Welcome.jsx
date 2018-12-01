69f6b58dc540da9d0309b1d4f7cdb0bc5e4bce70
Artur Chmaro <artur.chmaro@consileon.pl>
2018-12-01 16:07:33 +0100

    reactstrap

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,15 +1,24 @@
 import React, { Component } from 'react'
+import { Container, Row, Col, Button } from 'reactstrap'
+import { Link } from 'react-router-dom'
+
 
 export default class Welcome extends Component {
   render() {
-    
+
     return (
-      <div>
-        <h1>Welcome!</h1>
-        <a href="/sexchoice">
-          <button>Start</button>
-        </a>
-      </div>
+      <Container>
+        <Row>
+          <Col sm="12" md={{ size: 6, offset: 3 }}>
+            <h1>Welcome!</h1>
+            <a href='/user'>
+              <Button color="primary" size="lg" block>
+                Start
+              </Button>
+            </a>
+          </Col>
+        </Row>
+      </Container>
     )
   }
 }
