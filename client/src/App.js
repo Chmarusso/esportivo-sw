2167349f39dd593094a71a810ff1d50d40603740
Artur Chmaro <artur.chmaro@consileon.pl>
2018-12-01 16:29:17 +0100

    gradient, user screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import Welcome from './components/Welcome'
 import User from './components/User'
 import TournamentList from './components/TournamentsList'
 import QuizStartingScreen from './components/QuizStartingScreen'
@@ -14,8 +13,7 @@ class App extends Component {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path='/' component={Welcome} />
-          <Route exact path='/user' component={User} />
+          <Route exact path='/' component={User} />
           <Route exact path='/tournaments' component={TournamentList} />
           <Route exact path='/quizstart' component={QuizStartingScreen} />
           <Route exact path='/questions' component={Questions} />
